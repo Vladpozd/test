542717ff6a00e3f015114ef16c7acb1980a6d1b2
Vladpozd <103980547+Vladpozd@users.noreply.github.com>
2022-05-12 15:07:25 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int hour, rate, month, year,day;
+        hour=1;
+        rate=100;
+        day=hour*24;
+        month=day*30;
+        year=month*12;
+        int hour_wages = hour * rate;
+
+        System.out.print( hour_wages);
+        System.out.println(rate* day);
+        System.out.println(rate* month);
+        System.out.println(rate* year);
+
+
+    }
+}
